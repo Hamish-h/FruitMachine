39a8a7532c38c59f0f653b0cb16fdbd37aea4fff
Hanish-H <hamish.h@outlook.com>
2018-06-02 22:19:25 +0100

    teated selections from enums, apple orange pear

--- src/test/java/FruitMachineTest.java
+++ src/test/java/FruitMachineTest.java
@@ -13,8 +13,18 @@ public class FruitMachineTest {
     }
 
     @Test
-    public void canGetSelection(){
+    public void canGetSelectionApple(){
         assertEquals(Selection.APPLE, fruitMachine.getSelection(Selection.APPLE));
+    }
+
+    @Test
+    public void canGetSelectionOrange(){
+        assertEquals(Selection.ORANGE, fruitMachine.getSelection(Selection.ORANGE));
+    }
+
+    @Test
+    public void canGetSelectionPear(){
+        assertEquals(Selection.PEAR, fruitMachine.getSelection(Selection.PEAR));
     }
 
 }

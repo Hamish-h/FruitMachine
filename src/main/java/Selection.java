c55fa1230c5f4ea04b353206c386bfb89c7059fc
Hanish-H <hamish.h@outlook.com>
2018-06-02 23:06:55 +0100

    updated enums

--- src/main/java/Selection.java
+++ src/main/java/Selection.java
@@ -1,14 +1,7 @@
 public enum Selection {
 
-    APPLE("Apple"),
-    ORANGE("Orange"),
-    PEAR("Pear");
-
-
-    private String value;
-
-    Selection(String value) {
-        this.value = value;
-    }
+    APPLE,
+    ORANGE,
+    PEAR
 
 }

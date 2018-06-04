1c3cf749703a512d7e5c4eb8a5fbcd908d10ce7e
Hanish-H <hamish.h@outlook.com>
2018-06-04 23:22:19 +0100

    amended updated runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,45 +4,51 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        FruitMachine fruitMachine = new FruitMachine();
-        fruitMachine.generateAllChoices();
-        fruitMachine.getReels();
+//        FruitMachine fruitMachine = new FruitMachine();
+//        fruitMachine.generateAllChoices();
+//        fruitMachine.getReels();
+//
+//
+//    }
+//}
 
 
+        // User instructions
+        System.out.println("Enter 1 to play the game");
+
+        Scanner getPlayerInput = new Scanner(System.in);
+        Integer gamePlay = getPlayerInput.nextInt();
+
+        boolean game = true;
+
+        while (game = true) {
+
+            // User instructions
+            System.out.println("Enter 1 to play the game : Enter 2 to exit the game when running");
+
+            // player spins
+            if (gamePlay == 1){
+
+                // exit clause if no credit
+                if (playerCredits == 0){
+                    System.out.println("Credit Required - Game Over - Collect any Payout!!!");
+                    game = false;
+                }
+
+                // player spins reel
+                else if (playerCredits != 0){
+                    FruitMachine fruitMachine = new FruitMachine();
+                    fruitMachine.generateAllChoices();
+                    fruitMachine.getReels();
+                }
+            }
+
+            // player exits game
+            if (gamePlay == 2){
+                System.out.println("Game Over - Collect any Payout!!!");
+                game = false;
+
+            }
+        }
     }
 }
-
-//        Scanner getPlayerInput = new Scanner(System.in);
-//        Integer gamePlay = getPlayerInput.nextInt();
-//
-//        boolean game = true;
-//
-//
-//        while (game = true) {
-//
-//            // player spins
-//            if (gamePlay == 1){
-//
-//                // exit clause if no credit
-//                if (playerCredits == 0){
-//                    System.out.println("Credit Required - Game Over - Collect any Payout!!!");
-//                    game = false;
-//                }
-//
-//                // player spins reel
-//                else if (playerCredits != 0){
-//                    FruitMachine fruitMachine = new FruitMachine();
-//                    fruitMachine.generateAllChoices();
-//                    fruitMachine.getReels();
-//                }
-//            }
-//
-//            // player exits game
-//            if (gamePlay == 2){
-//                System.out.println("Game Over - Collect any Payout!!!");
-//                game = false;
-//
-//            }
-//        }
-//  }
-//}

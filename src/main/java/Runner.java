3e202e8d3be268c48c78758962fe789da1748ced
Hanish-H <hamish.h@outlook.com>
2018-06-07 11:53:11 +0100

    tiding code DRY

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -8,22 +8,22 @@ public class Runner {
         Player player = new Player();
 
         // runner messages for player
-        String line00 = "==============================";
-        String line01 = "|   CODECLANS FRUIT MACHINE   |";
-        String line02 = "|     £1  Credit per play     |";
-        String line03 = "=======  BUY CREDITS  ========";
-        String line04 = "   1 = play    2 = winnings!";
-        String line05 = " CREDIT REFUND & WINNINGS £";
-        String line06 = "You have £";
-        String line07 = " on your VisaCard!";
-        String line08 = "Data entered invalid, Game Over!";
-        String line09 = "That exceeds your VisaCard credit!";
-        String line10 = "Invalid amount, £1 credit per play!";
-        String line11 = "------------------------------";
-        String line12 = "|  APPLE 3x£30 4x£90 5x£500!  |";
-        String line13 = "|  PEARS 3x£30 4x£90 5x£500!  |";
-        String line14 = "|  LIMES 3x£30 4x£90 5x£500!  |";
-        String line15 = "|     * * * PAYOUT * * *      |";
+        String displayDoubleLine = "==============================";
+        String displaySingleLine = "------------------------------";
+        String displayFruitMachine = "|   CODECLANS FRUIT MACHINE   |";
+        String displayLineApples = "|  APPLE 3x£30 4x£90 5x£500!  |";
+        String displayPears = "|  PEARS 3x£30 4x£90 5x£500!  |";
+        String displayLimes = "|  LIMES 3x£30 4x£90 5x£500!  |";
+        String displayPayout = "|     * * * PAYOUT * * *      |";
+        String displayCreditPerPlay = "|     £1  Credit per play     |";
+        String displayBuyCredits = "=======  BUY CREDITS  ========";
+        String displayPlayerOptions = "   1 = play   2 = winnings!";
+        String displayCreditRefundWinnings = " CREDIT REFUND & WINNINGS £";
+        String displayYouHave = "You have £";
+        String displayOnYourVisa = " on your VisaCard!";
+        String displayDataInvalidGameOver = "Data entered invalid, Game Over!";
+        String displayExceedsVisaCredit = "That exceeds your VisaCard credit!";
+        String displayInvalidAmount = "Invalid amount, £1 credit per play!";
 
         // runner values
         Integer PlayerVisaCard;
@@ -36,13 +36,13 @@ public class Runner {
         java.util.Date date = new java.util.Date();
 
         // player instructions
-        System.out.println(line00 + "\n" + line01 + "\n" + line12 + "\n" + line13 + "\n" + line14 + "\n" + line11 + "\n" + line02);
+        System.out.println(displayDoubleLine + "\n" + displayFruitMachine + "\n" + displayLineApples + "\n" + displayPears + "\n" + displayLimes + "\n" + displaySingleLine + "\n" + displayCreditPerPlay);
 
         // players wallet contents message
-        System.out.println(line11);
-        System.out.println(line06 + player.visaCard() + line07);
-        System.out.println(line11 + "\n" + line03);
-        //     System.out.println(line03);
+        System.out.println(displaySingleLine);
+        System.out.println(displayYouHave + player.visaCard() + displayOnYourVisa);
+        System.out.println(displaySingleLine + "\n" + displayBuyCredits);
+        //     System.out.println(displayBuyCredits);
 
         // get player input
         Scanner getPlayerMoney = new Scanner(System.in);
@@ -50,13 +50,13 @@ public class Runner {
 
         // exit if player input exceeds credit card limit
         if (playerCash > 100){
-            System.out.println(line09);
+            System.out.println(displayExceedsVisaCredit);
             runGame = false;
         }
 
         // exit if player enters negative or zero value
         if (playerCash <= 0){
-            System.out.println(line10);
+            System.out.println(displayInvalidAmount);
             runGame = false;
         }
 
@@ -70,9 +70,9 @@ public class Runner {
             PlayerVisaCard = (player.visaCard() - playerCash);
 
             // Set player instructions
-            System.out.println(line11);
-            System.out.println(line06 + PlayerVisaCard + line07);
-            System.out.println(line11 + "\n" + line04 + "\n" + line11);
+            System.out.println(displaySingleLine);
+            System.out.println(displayYouHave + PlayerVisaCard + displayOnYourVisa);
+            System.out.println(displaySingleLine + "\n" + displayPlayerOptions + "\n" + displaySingleLine);
 
             // Get player input
             Scanner getPlayerInput = new Scanner(System.in);
@@ -80,7 +80,7 @@ public class Runner {
 
             // exit if player enters any other characters
             if (playerInput != 1 && playerInput != 2) {
-                System.out.println(line08);
+                System.out.println(displayDataInvalidGameOver);
                 runGame = false;
             }
 
@@ -96,11 +96,11 @@ public class Runner {
 
                 // set output payout
 
-                System.out.println(line11 + "\n" + line15 + "\n" + line01 + "\n" + line11);
-                System.out.println(line05 + playerPayout);
-                System.out.println(line11);
-                System.out.println(line06 + playerTally + line07);
-                System.out.println(line11 + "\n" + date + "\n" + line11);
+                System.out.println(displaySingleLine + "\n" + displayPayout + "\n" + displayFruitMachine + "\n" + displaySingleLine);
+                System.out.println(displayCreditRefundWinnings + playerPayout);
+                System.out.println(displaySingleLine);
+                System.out.println(displayYouHave + playerTally + displayOnYourVisa);
+                System.out.println(displaySingleLine + "\n" + date + "\n" + displaySingleLine);
 
 
                 // spacer
@@ -125,11 +125,11 @@ public class Runner {
                         System.out.println();
 
                     // set output payout
-                    System.out.println(line11 + "\n" + line15 + "\n" + line01 + "\n" + line11);
-                    System.out.println(line05 + playerPayout);
-                    System.out.println(line11);
-                    System.out.println(line06 + playerTally + line07);
-                    System.out.println(line11 + "\n" + date + "\n" + line11);
+                    System.out.println(displaySingleLine + "\n" + displayPayout + "\n" + displayFruitMachine + "\n" + displaySingleLine);
+                    System.out.println(displayCreditRefundWinnings + playerPayout);
+                    System.out.println(displaySingleLine);
+                    System.out.println(displayYouHave + playerTally + displayOnYourVisa);
+                    System.out.println(displaySingleLine + "\n" + date + "\n" + displaySingleLine);
 
                     // spacer
                     for (lines = 0; lines <= 2; lines++)
